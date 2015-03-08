8a3b531c3392086fa80e55f1046e1c930217c833
Steven Hoffman <git@fustrate.com>
2015-03-08 14:52:46 -0700

    nginx & unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,21 @@
+app_name = 'fustrate.com'
+app_dir = "/home/steven/apps/#{app_name}"
+
+# Set the working application directory
+working_directory app_dir
+
+# Unicorn PID file location
+pid "#{app_dir}/pids/unicorn.pid"
+
+# Path to logs
+stderr_path "#{app_dir}/log/unicorn.log"
+stdout_path "#{app_dir}/log/unicorn.log"
+
+# Unicorn socket
+listen "/tmp/unicorn.#{app_name}.sock"
+
+# Number of processes
+worker_processes 2
+
+# Time-out
+timeout 30
