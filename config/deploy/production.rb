6904c0707a2de4a474da48367e5c1f2995a51df4
Steven Hoffman <git@fustrate.com>
2015-03-09 20:39:34 -0700

    moar unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,6 @@ server '173.255.247.137', user: 'baseballbot', roles: %w(web app db)
 
 set :stage, :production
 
-set :nginx_server_name, 'baseballbot.io'
-
 set :ssh_options,
     user: 'baseballbot',
     keys: %w(/Users/steven/.ssh/id_rsa.pub),
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,14 +7,14 @@ app_dir = "/home/#{app_user}/apps/#{app_name}"
 working_directory app_dir
 
 # Unicorn PID file location
-pid "#{app_dir}/tmp/pids/unicorn.pid"
+pid "#{app_dir}/shared/tmp/pids/unicorn.pid"
 
 # Path to logs
-stderr_path "#{app_dir}/log/unicorn.log"
-stdout_path "#{app_dir}/log/unicorn.log"
+stderr_path "#{app_dir}/shared/log/unicorn.error.log"
+stdout_path "#{app_dir}/shared/log/unicorn.log"
 
 # Unicorn socket
-listen "#{app_dir}/tmp/sockets/unicorn.sock"
+listen "#{app_dir}/shared/tmp/sockets/unicorn.sock"
 
 # Number of processes
 worker_processes 2
