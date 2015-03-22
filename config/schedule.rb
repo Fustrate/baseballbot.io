b8643fb290770c6b72fd480d21dea8d4aa14eb4c
Steven Hoffman <git@fustrate.com>
2015-03-22 09:34:42 -0700

    activate the game chat posting!
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,12 +1,16 @@
 set :output, '/home/baseballbot/apps/baseballbot.io/shared/log/whenever.log'
 
-# every 2.minutes do
-# end
-
-# every 10.minutes do
-# end
-
 every 1.hour do
   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
           'bundle exec ruby update_sidebars.rb'
 end
+
+every 10.minutes do
+  command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
+          'bundle exec ruby post_gamechats.rb'
+end
+
+# every 2.minutes do
+#   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
+#           'bundle exec ruby update_gamechats.rb'
+# end
