e757db3eba2011b9230252632a1c2718deadc071
Steven Hoffman <git@fustrate.com>
2015-03-28 15:55:13 -0700

    Remove TEAM@TEAM from beginning of highlights
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/gamechat.rb
+++ lib/baseballbot/template/gamechat.rb
@@ -200,7 +200,8 @@ class Baseballbot
             highlights << {
               team: media['team_id'].to_i == team.id ? team : opponent,
               headline: media.at_xpath('headline').text.strip,
-              blurb: media.at_xpath('blurb').text.strip,
+              blurb: media.at_xpath('blurb').text.strip
+                .gsub(/^[A-Z@]+: /, ''),
               duration: media.at_xpath('duration').text.strip
                 .gsub(/^00:0?/, ''),
               url: media.at_xpath('url').text.strip
