5eaf1e7711a53714cd14a9518c30f7a9fa38919f
Steven Hoffman <git@fustrate.com>
2015-04-07 14:39:45 -0700

    Start linking to gamechats in the baseball sidebar
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/sidebar/todays_games.rb
+++ lib/baseballbot/template/sidebar/todays_games.rb
@@ -7,21 +7,32 @@ class Baseballbot
         def todays_games
           games = Nokogiri::XML open time.now.strftime SCOREBOARD_URL
 
+          load_gamechats
+
           games.xpath('//games/game').map do |game|
+            home_code = game.xpath('@home_name_abbrev').text
+            home_subreddit = subreddit home_code
+
+            away_code = game.xpath('@away_name_abbrev').text
+            away_subreddit = subreddit away_code
+
+            gid = game.xpath('@gameday_link').text
+
             {
-              home: link_for_team(game.xpath('@home_name_abbrev').text),
-              away: link_for_team(game.xpath('@away_name_abbrev').text),
-              time: game.xpath('@time').text
+              gid: game.xpath('@gameday_link').text,
+              time: game.xpath('@time').text,
+              home: link_for_team(code: home_code,
+                                  subreddit: home_subreddit,
+                                  gid: gid),
+              away: link_for_team(code: away_code,
+                                  subreddit: away_subreddit,
+                                  gid: gid)
             }
           end
         end
 
-        def link_for_team(code)
-          return "[#{code}][#{code}]"
-
-          @gamechats ||= load_gamechats
-
-          gamechat = @gamechats[subreddit(code).downcase]
+        def link_for_team(code:, subreddit:, gid:)
+          gamechat = @gamechats["#{gid}_#{subreddit}".downcase]
 
           if gamechat
             "[#{code} ^★](/#{gamechat} \"team-#{code.downcase}\")"
@@ -30,73 +41,73 @@ class Baseballbot
           end
         end
 
+        protected
+
+        def load_gamechats
+          @gamechats = {}
+
+          # bots = {
+          #   astros:           'astrosbot',
+          #   nyyankees:        'yankeebot',
+          #   phillies:         'philsbot',
+          #   azdiamondbacks:   'snakebot',
+          #   minnesotatwins:   'twinsgameday',
+          #   motorcitykitties: 'tigersbot',
+          #   kcroyals:         'royalsbot',
+          #   padres:           'friarbot',
+          #   orioles:          'osgamethreads',
+          #   newyorkmets:      'metsbot',
+          #   whitesox:         'chisoxbot',
+          #   # Uses their own account
+          #   cardinals:        'bravo_delta AND title:Game Cardinals',
+          #   # Multiple authors
+          #   buccos:           ' AND title:GDT',
+          #   # reddit doesn't like this user as an author
+          #   braves:           ' AND title:Game Thread Chief_Noc-A-Homa',
+          # }
+          #
+          # queries = ['selftext:hellobaseballbot self:yes']
+
+          @bot.redis.keys(time.now.strftime '%Y_%m_%d_*').each do |gid|
+            @bot.redis.hgetall(gid).each do |subreddit, link_id|
+              @gamechats["#{gid}_#{subreddit}".downcase] = link_id
+              # bots.delete subreddit.to_sym
+            end
+          end
+
+          # bots.map do |subreddit, bot|
+          #   "(subreddit:#{subreddit} AND author:#{bot})"
+          # end.each_slice(8) do |group|
+          #   queries.unshift group.join ' OR '
+          # end
+          #
+          # queries.each do |query|
+          #   data = load_gamechats_from_query query
+          #
+          #   sleep 0.5
+          #
+          #   data.each do |item|
+          #     date = Time.at(
+          #       item['data']['created_utc'].to_i + 10_800
+          #     ).to_datetime
+          #
+          #     next unless date.day == Date.today.day
+          #
+          #     subreddit = item['data']['subreddit'].downcase
+          #     link_id = item['data']['id']
+          #
+          #     redis.hset hash_name, subreddit, link_id
+          #
+          #     gamechats[subreddit] ||= link_id
+          #   end
+          # end
+        end
+
         def load_gamechats_from_query(query)
           url = 'http://www.reddit.com/user/BaseballBot/m/teams/search.json?' \
                 "q=#{CGI.escape query}&sort=new&restrict_sr=on&limit=50&t=day"
 
           JSON.parse(open(url).read)['data']['children']
-        end
-
-        def load_gamechats
-          gamechats = {}
-
-          bots = {
-            astros:           'astrosbot',
-            nyyankees:        'yankeebot',
-            phillies:         'philsbot',
-            azdiamondbacks:   'snakebot',
-            minnesotatwins:   'twinsgameday',
-            motorcitykitties: 'tigersbot',
-            kcroyals:         'royalsbot',
-            padres:           'friarbot',
-            orioles:          'osgamethreads',
-            newyorkmets:      'metsbot',
-            whitesox:         'chisoxbot',
-            # Uses their own account
-            cardinals:        'bravo_delta AND title:Game Cardinals',
-            # Multiple authors
-            buccos:           ' AND title:GDT',
-            # reddit doesn't like this user as an author
-            braves:           ' AND title:Game Thread Chief_Noc-A-Homa',
-          }
-
-          queries = ['selftext:hellobaseballbot self:yes']
-
-          hash_name = @time.strftime 'gamechats_%Y%m%d'
-
-          @bot.redis.hgetall(hash_name).each do |subreddit, link_id|
-            gamechats[subreddit] = link_id
-            bots.delete subreddit.to_sym
-          end
-
-          bots.map do |subreddit, bot|
-            "(subreddit:#{subreddit} AND author:#{bot})"
-          end.each_slice(8) do |group|
-            queries.unshift group.join ' OR '
-          end
-
-          queries.each do |query|
-            data = load_gamechats_from_query query
-
-            sleep 0.5
-
-            data.each do |item|
-              date = Time.at(
-                item['data']['created_utc'].to_i + 10_800
-              ).to_datetime
-
-              next unless date.day == Date.today.day
-
-              subreddit = item['data']['subreddit'].downcase
-              link_id = item['data']['id']
-
-              redis.hset hash_name, subreddit, link_id
-
-              gamechats[subreddit] ||= link_id
-            end
-          end
-
-          gamechats
         end
       end
     end
