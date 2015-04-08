ccd536c4db4daf2575fc598e8686b8c6bed093f6
Steven Hoffman <git@fustrate.com>
2015-04-08 11:02:15 -0700

    Update the /r/baseball sidebar every 5 minutes
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,6 +10,12 @@ every 5.minutes do
           'bundle exec ruby post_gamechats.rb'
 end
 
+# So we don't run twice on the hour
+every '5,10,15,20,25,30,35,40,45,50,55 * * * *' do
+  command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
+          'bundle exec ruby update_sidebars.rb baseball'
+end
+
 every 2.minutes do
   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
           'bundle exec ruby update_gamechats.rb'
