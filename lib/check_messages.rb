ac4134b89e06b9a2dd50d153056a4c27477f8b1c
Steven Hoffman <git@fustrate.com>
2015-04-09 23:45:26 -0700

    Try to determine games without GID in post
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/check_messages.rb
+++ lib/check_messages.rb
@@ -1,6 +1,9 @@
 require_relative 'baseballbot'
 
-bot = Baseballbot.new(
+SCOREBOARD_URL = 'http://gd2.mlb.com/components/game/mlb/year_%Y/month_%m/' \
+                 'day_%d/miniscoreboard.xml'
+
+@bot = Baseballbot.new(
   reddit: {
     user_agent: 'Baseballbot by /u/Fustrate',
     client_id: ENV['REDDIT_CLIENT_ID'],
@@ -19,25 +22,73 @@ TITLE = /(?:game ?(?:thread|chat|day)|gdt)/i
 LINK = %r{(?:redd\.it|/comments|reddit\.com)/([a-z0-9]{6})}i
 GID = /(?:gid_)?(\d{4}_\d{2}_\d{2}_[a-z]{6}_[a-z]{6}_\d)/
 
-client = bot.clients['BaseballBot']
-account = bot.accounts.select { |_, a| a.name == 'BaseballBot' }.values.first
+def client
+  baseballbot = @bot.clients['BaseballBot']
+  account = @bot.accounts.select { |_, a| a.name == 'BaseballBot' }.values.first
 
-client.access = account.access
-bot.refresh_client!(client) if account.access.expired?
+  baseballbot.access = account.access
+  @bot.refresh_client!(baseballbot) if account.access.expired?
 
-client.my_messages('unread', false, limit: 10).each do |pm|
-  next unless pm.subject =~ TITLE && pm.body =~ LINK
+  baseballbot
+end
+
+def process_message(message)
+  return unless message.subject =~ TITLE && message.body =~ LINK
 
   post_id = Regexp.last_match[1]
 
   submission = client.from_fullname("t3_#{post_id}").first
 
-  next unless submission[:selftext] =~ GID
-
   subreddit = submission[:subreddit].downcase
-  gid = Regexp.last_match[1]
 
-  bot.redis.hset gid, subreddit, post_id
+  if submission[:selftext] =~ GID
+    gid = Regexp.last_match[1]
+  else
+    gid = find_possible_game(subreddit, post_id)
+  end
 
-  pm.mark_as_read
+  @bot.redis.hset gid, subreddit, post_id if gid
+
+  message.mark_as_read
 end
+
+def find_possible_game(subreddit, post_id)
+  gids = possible_games[@bot.subreddit_to_code subreddit]
+  possibilities = []
+
+  # Remove GIDs that have already been sent to us
+  gids.each do |gid|
+    existing_id = @bot.redis.hget gid, subreddit
+
+    # This post_id is already in the system, so make sure `first` returns nil
+    if existing_id == post_id
+      possibilities.unshift nil
+      break
+    end
+
+    possibilities << gid if existing_id
+  end
+
+  possibilities.first
+end
+
+def possible_games
+  @possible_games ||= load_possible_games
+end
+
+def load_possible_games
+  games = Hash.new []
+
+  Nokogiri::XML(open(Date.now.strftime SCOREBOARD_URL))
+    .xpath('//games/game')
+    .map do |game|
+      gid = game.xpath('@gameday_link').text
+
+      games[game.xpath('@home_name_abbrev').text] << gid
+      games[game.xpath('@away_name_abbrev').text] << gid
+    end
+
+  games
+end
+
+client.my_messages('unread', false, limit: 5).each { |msg| process_message msg }
