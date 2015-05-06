f81e0b4f94e2048498e95559069edc9d6ac1aa68
Steven Hoffman <git@fustrate.com>
2015-05-05 23:41:01 -0700

    Move live debugging
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # Home page, full of nothing right now
 class HomeController < ApplicationController
   def home
-    Rails.logger.info ENV.keys.inspect if params[:debug]
+    Rails.logger.info ENV.inspect if params[:debug]
 
     @gamechats = Gamechat
                  .where('DATE(post_at) = ?', Time.zone.today)
