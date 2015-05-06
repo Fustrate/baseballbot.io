78154df34f7014fb75982fd07096005373284480
Steven Hoffman <git@fustrate.com>
2015-05-05 23:36:12 -0700

    A bit of live debugging
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 # Home page, full of nothing right now
 class HomeController < ApplicationController
   def home
+    Rails.logger.info ENV.keys.inspect if params[:debug]
+
     @gamechats = Gamechat
                  .where('DATE(post_at) = ?', Time.zone.today)
                  .includes(:subreddit)
