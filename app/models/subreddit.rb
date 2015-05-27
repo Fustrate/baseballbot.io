00c0f5cbc646a7e22098c9c337ffb7ea5482a7a8
Steven Hoffman <shoffman@valenciamgmt.com>
2015-05-27 11:33:36 -0700

    More info on the subreddits page
    Signed-off-by: Steven Hoffman <shoffman@valenciamgmt.com>

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -5,4 +5,20 @@ class Subreddit < ActiveRecord::Base
   def url
     "http://reddit.com/r/#{name}"
   end
+
+  def update_sidebar?
+    @update_sidebar ||= subreddit.options['sidebar'].try(:[], 'enabled')
+  end
+
+  def post_gamechats?
+    @post_gamechats ||= subreddit.options['gamechats'].try(:[], 'enabled')
+  end
+
+  def post_pregame?
+    @post_pregame ||= subreddit.options['pregame'].try(:[], 'enabled')
+  end
+
+  def post_postgame?
+    @post_postgame ||= subreddit.options['postgame'].try(:[], 'enabled')
+  end
 end
