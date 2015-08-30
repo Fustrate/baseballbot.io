cae3a49b3a6edd18fbb5047bdd25451f7431ee37
Steven Hoffman <git@fustrate.com>
2015-08-30 11:10:13 -0700

    line score refactoring
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/gamechat/line_score.rb
+++ lib/baseballbot/template/gamechat/line_score.rb
@@ -2,40 +2,18 @@ class Baseballbot
   module Template
     class Gamechat
       module LineScore
+        BLANK_RHE = { runs: 0, hits: 0, errors: 0 }
+
         def home
-          unless @game.started? && @game.boxscore
-            return {
-              runs: 0,
-              hits: 0,
-              errors: 0
-            }
-          end
+          return BLANK_RHE unless @game.started? && @game.boxscore
 
-          rhe = @game.boxscore.at_xpath '//boxscore/linescore'
-
-          {
-            runs: rhe['home_team_runs'].to_i,
-            hits: rhe['home_team_hits'].to_i,
-            errors: rhe['home_team_errors'].to_i
-          }
+          rhe_for_side 'home'
         end
 
         def away
-          unless @game.started? && @game.boxscore
-            return {
-              runs: 0,
-              hits: 0,
-              errors: 0
-            }
-          end
+          return BLANK_RHE unless @game.started? && @game.boxscore
 
-          rhe = @game.boxscore.at_xpath '//boxscore/linescore'
-
-          {
-            runs: rhe['away_team_runs'].to_i,
-            hits: rhe['away_team_hits'].to_i,
-            errors: rhe['away_team_errors'].to_i
-          }
+          rhe_for_side 'away'
         end
 
         def lines
@@ -43,45 +21,59 @@ class Baseballbot
 
           return lines unless @game.started? && @game.boxscore
 
-          bs = @game.boxscore
-
-          bs.xpath('//boxscore/linescore/inning_line_score').each do |inning|
-            if inning['away'] && !inning['away'].empty?
-              lines[0][inning['inning'].to_i - 1] = inning['away']
-
-              # In case of extra innings
-              lines[1][inning['inning'].to_i - 1] = nil
-            end
-
-            if inning['home'] && !inning['home'].empty?
-              lines[1][inning['inning'].to_i - 1] = inning['home']
-            end
-          end
+          @game.boxscore
+            .xpath('//boxscore/linescore/inning_line_score')
+            .each { |inning| add_inning_to_line_score(inning, lines: lines) }
 
           lines
         end
 
         def line_score
           [
-            " |#{ (1..(lines[0].count)).to_a.join('|') }|R|H|E",
-            ":-:|#{ (':-:|' * lines[0].count) }:-:|:-:|:-:",
-            "[#{ game.away_team.code }](/#{ game.away_team.code })|" \
-              "#{ lines[0].join('|') }|#{ bold away[:runs] }|" \
-              "#{ bold away[:hits] }|#{ bold away[:errors] }",
-            "[#{ game.home_team.code }](/#{ game.home_team.code })|" \
-              "#{ lines[1].join('|') }|#{ bold home[:runs] }|" \
-              "#{ bold home[:hits] }|#{ bold home[:errors] }"
+            " |#{(1..(lines[0].count)).to_a.join('|')}|R|H|E",
+            ":-:|#{(':-:|' * lines[0].count)}:-:|:-:|:-:",
+            "[#{game.away_team.code}](/#{game.away_team.code})|" \
+              "#{lines[0].join('|')}|#{bold away[:runs]}|" \
+              "#{bold away[:hits]}|#{bold away[:errors]}",
+            "[#{game.home_team.code}](/#{game.home_team.code})|" \
+              "#{lines[1].join('|')}|#{bold home[:runs]}|" \
+              "#{bold home[:hits]}|#{bold home[:errors]}"
           ].join "\n"
         end
 
         def line_score_status
-          if game.over?
-            'Final'
-          elsif runners.empty?
-            "#{outs} #{outs == 1 ? 'Out' : 'Outs'}, #{inning}"
-          else
-            "#{runners}, #{outs} #{outs == 1 ? 'Out' : 'Outs'}, #{inning}"
+          return 'Final' if game.over?
+
+          if runners.empty?
+            return "#{outs} #{outs == 1 ? 'Out' : 'Outs'}, #{inning}"
           end
+
+          "#{runners}, #{outs} #{outs == 1 ? 'Out' : 'Outs'}, #{inning}"
+        end
+
+        protected
+
+        def rhe_for_side(side)
+          @rhe ||= @game.boxscore.at_xpath '//boxscore/linescore'
+
+          {
+            runs: @rhe["#{side}_team_runs"].to_i,
+            hits: @rhe["#{side}_team_hits"].to_i,
+            errors: @rhe["#{side}_team_errors"].to_i
+          }
+        end
+
+        def add_inning_to_line_score(inning, lines:)
+          if inning['away'] && !inning['away'].empty?
+            lines[0][inning['inning'].to_i - 1] = inning['away']
+
+            # In case of extra innings
+            lines[1][inning['inning'].to_i - 1] = nil
+          end
+
+          return unless inning['home'] && !inning['home'].empty?
+
+          lines[1][inning['inning'].to_i - 1] = inning['home']
         end
       end
     end
