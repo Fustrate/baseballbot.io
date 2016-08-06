ae4207d0ab5328cd63510f6a6e6a969c36d3f6c5
Steven Hoffman <git@fustrate.com>
2016-08-05 22:24:55 -0700

    better scheduling
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,6 +4,12 @@ set :output, '/home/baseballbot/apps/baseballbot.io/shared/log/whenever.log'
 every 1.hour do
   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
           'bundle exec ruby update_sidebars.rb'
+end
+
+every 15.minutes do
+  command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
+          'bundle exec ruby check_messages.rb'
+
   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
           'bundle exec ruby post_pregames.rb'
 end
@@ -22,9 +28,4 @@ end
 every 2.minutes do
   command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
           'bundle exec ruby update_gamechats.rb'
-end
-
-every 15.minutes do
-  command 'cd /home/baseballbot/apps/baseballbot.io/current/lib && ' \
-          'bundle exec ruby check_messages.rb'
 end
