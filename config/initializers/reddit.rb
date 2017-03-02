e5e211f2516bdc7b89363af8b71da4925fe82d82
Steven Hoffman <fustrate@me.com>
2017-03-02 12:07:16 -0800

    rewind Redd
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- config/initializers/reddit.rb
+++ config/initializers/reddit.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 def reddit
-  @reddit ||= Redd.it :web,
-                      Rails.application.secrets.reddit['client_id'],
-                      Rails.application.secrets.reddit['secret'],
-                      Rails.application.secrets.reddit['redirect_uri']
+  @reddit ||= Redd.it(
+    :web,
+    Rails.application.secrets.reddit['client_id'],
+    Rails.application.secrets.reddit['secret'],
+    Rails.application.secrets.reddit['redirect_uri']
+  )
 end
