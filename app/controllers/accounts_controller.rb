bfe79e90e132a14c1b54005843398d0720975f74
Steven Hoffman <git@fustrate.com>
2017-04-21 21:14:08 -0700

    more controller actions
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -16,6 +16,24 @@ class AccountsController < ApplicationController
   def index
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   def authenticate
     if params[:error]
       flash[:error] = "Reddit returned an error: #{params[:error]}"
