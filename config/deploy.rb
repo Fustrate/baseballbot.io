9bbedc40d01ddf7cfe35fb5cc90b0dd6e33b27ed
Steven Hoffman <git@fustrate.com>
2017-04-21 19:07:18 -0700

    gem updates
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # config valid only for current version of Capistrano
-lock '3.8.0'
+lock '3.8.1'
 
 set :application, 'baseballbot.io'
 set :user, 'baseballbot'
@@ -11,9 +11,9 @@ set :branch, ENV['REVISION'] || :master
 
 set :deploy_to, "/home/#{fetch :user}/apps/#{fetch :application}"
 
-set :linked_files, %w(config/secrets.yml config/database.yml
-                      config/honeybadger.yml config/skylight.yml)
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets public/system)
+set :linked_files, %w[config/secrets.yml config/database.yml
+                      config/honeybadger.yml config/skylight.yml]
+set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets public/system]
 
 set :default_env, path: '/opt/ruby/bin:$PATH'
 
@@ -21,7 +21,7 @@ set :rbenv_ruby,
     File.read(File.expand_path('../../.ruby-version', __FILE__)).strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch :rbenv_path} " \
                    "#{fetch :rbenv_path}/bin/rbenv exec"
-set :rbenv_map_bins, %w(rake gem bundle ruby rails honeybadger)
+set :rbenv_map_bins, %w[rake gem bundle ruby rails honeybadger]
 
 namespace :deploy do
   after :publishing, 'unicorn:reload'
