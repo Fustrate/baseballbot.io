83b8502a61c9129b817b09a206a5b1c49b9737fa
Steven Hoffman <git@fustrate.com>
2018-03-10 20:05:53 -0800

    highlights w/ SD & HD options
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/gamechat/highlights.rb
+++ lib/baseballbot/template/gamechat/highlights.rb
@@ -5,14 +5,12 @@ class Baseballbot
     class Gamechat
       module Highlights
         def highlights
-          []
+          return [] unless started?
 
-          # return [] unless started?
-          #
-          # Nokogiri::XML(open_file('media/mobile.xml'))
-          #   .xpath('//highlights/media')
-          #   .sort_by { |a| a['date'] }
-          #   .map { |media| process_media(media) }
+          content.dig('highlights', 'live', 'items')
+            .sort_by { |media| media['id'] }
+            .map { |media| process_media(media) }
+            .compact
         end
 
         def highlights_list
@@ -20,7 +18,7 @@ class Baseballbot
             .map do |highlight|
               icon = link_to '', url: "/#{highlight[:team].code}"
               text = "#{highlight[:blurb]} (#{highlight[:duration]})"
-              url = highlight[:url]
+              url = highlight[:hd]
 
               "- #{icon} #{url ? link_to(text, url: url) : text}"
             end
@@ -29,15 +27,24 @@ class Baseballbot
 
         protected
 
+        def playback(media, name)
+          media['playbacks'].find { |video| video['name'] == name }&.dig('url')
+        end
+
         def process_media(media)
-          url = media.at_xpath('url[@playback-scenario="FLASH_1200K_640X360"]')
+          return unless media['type'] == 'video'
+
+          team_id = media['keywordsDisplay']
+            .find { |keyword| keyword['type'] == 'team_id' }
+            &.dig('value')
 
           {
-            team: media['team_id'].to_i == team.id ? team : opponent,
-            headline: media.at_xpath('headline').text.strip,
-            blurb: media.at_xpath('blurb').text.strip.gsub(/^[A-Z@]+: /, ''),
-            duration: media.at_xpath('duration').text.strip.gsub(/^00:0?/, ''),
-            url: url&.text&.strip&.gsub('1200K', '1800K')
+            team: team_id.to_i == team.id ? team : opponent,
+            headline: media['headline'].strip,
+            blurb: media['blurb'].strip.gsub(/^[A-Z@]+: /, ''),
+            duration: media['duration'].strip.gsub(/^00:0?/, ''),
+            sd: playback(media, 'FLASH_1200K_640X360'),
+            hd: playback(media, 'FLASH_2500K_1280X720')
           }
         end
       end
