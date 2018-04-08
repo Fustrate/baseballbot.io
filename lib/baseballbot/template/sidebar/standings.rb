e0af8ed8c91b6be75a88b709b45151509607739e
Steven Hoffman <git@fustrate.com>
2018-04-08 13:04:08 -0700

    ugh
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/sidebar/standings.rb
+++ lib/baseballbot/template/sidebar/standings.rb
@@ -4,157 +4,157 @@ class Baseballbot
   module Template
     class Sidebar
       module Standings
-        STANDINGS = 'http://mlb.mlb.com/lookup/json/named.standings_schedule_' \
-                    'date.bam?season=%Y&schedule_game_date.game_date=\'' \
-                    '%Y/%m/%d\'&sit_code=\'h0\'&league_id=103&league_id=104' \
-                    '&all_star_sw=\'N\'&version=2'
+        STATS_STANDINGS = \
+          'https://statsapi.mlb.com/api/v1/standings/regularSeason?' \
+          'leagueId=103,104&season=%<year>d&date=%<date>s'
 
-        def divisions
-          @divisions ||= begin
-            teams = {}
+        def all_teams
+          return @all_teams if @all_teams
 
-            load_teams_from_remote.each do |team|
-              teams[team['team_abbrev'].to_sym] = parse_standings_row(team)
-            end
+          @all_teams = []
 
-            determine_wildcards teams
-
-            sort_teams_into_divisions(teams).each_value do |teams_in_division|
-              teams_in_division.sort_by! { |team| team[:sort_order] }
+          load_divisions_from_remote.each do |division|
+            division['teamRecords'].each do |team|
+              @all_teams << parse_standings_row(team)
             end
           end
+
+          @all_teams.sort_by! { |team| team[:sort_order] }
+
+          [103, 104].each { |league_id| mark_league_wildcards(league_id) }
+
+          @all_teams
         end
 
         def standings
-          divisions[@team.division.id]
+          teams_in_division(@team.division_id)
         end
 
         def full_standings
           @full_standings ||= {
-            nl: divisions[203].zip(divisions[205], divisions[204]),
-            al: divisions[200].zip(divisions[202], divisions[201])
+            al: teams_in_division(200).zip(
+              teams_in_division(202),
+              teams_in_division(201)
+            ),
+            nl: teams_in_division(203).zip(
+              teams_in_division(205),
+              teams_in_division(204)
+            )
           }
         end
-
-        def leagues
-          @leagues ||= {
-            nl: divisions[203] + divisions[204] + divisions[205],
-            al: divisions[200] + divisions[201] + divisions[202]
-          }
-        end
+        alias leagues full_standings
 
         def draft_order
-          @draft_order ||= divisions.values
-            .flatten(1)
+          @draft_order ||= all_teams
             .sort_by! { |team| team[:sort_order] }
             .reverse
         end
 
-        def wildcards_in_league(league)
-          wildcard_order(league).reject { |team| team[:games_back].zero? }
+        def teams_in_league(league_id)
+          all_teams.select { |team| team[:team].league_id == league_id }
         end
 
-        def wildcard_order(league)
-          leagues[league].sort_by { |team| team[:wildcard_gb] }
+        def teams_in_division(division_id)
+          all_teams.select { |team| team[:team].division_id == division_id }
         end
 
-        def determine_wildcards(teams)
-          determine_league_wildcards teams, [203, 204, 205]
-          determine_league_wildcards teams, [200, 201, 202]
-        end
-
-        def determine_league_wildcards(teams, division_ids)
-          eligible = teams_in_divisions(teams, division_ids)
-            .sort_by { |team| team[:wildcard_gb] }
-
-          first_and_second_wildcards(eligible)
-            .each_with_index do |teams_in_spot, position|
-              teams_in_spot.each do |team|
-                teams[team[:code].to_sym][:wildcard_position] = position + 1
-              end
-            end
+        def wildcards_in_league(league_id)
+          teams_in_league(league_id)
+            .reject { |team| team[:games_back] == '-' }
+            .sort_by! { |team| team[:wildcard_gb].to_i }
         end
 
         def team_stats
-          @team_stats ||= standings.find { |team| team[:code] == @team.code }
-        end
-
-        def [](stat)
-          team_stats[stat]
+          @team_stats ||= all_teams.find { |team| team[:team].id == @team.id }
         end
 
         protected
 
         # rubocop:disable Metrics/MethodLength
         def parse_standings_row(row)
+          team = @bot.api.team(row['team']['id'])
+
+          records = row.dig('records', 'splitRecords')
+            .map { |rec| [rec['type'], [rec['wins'], rec['losses']]] }
+            .to_h
+
           {
-            code:           row['team_abbrev'],
-            division_champ: %w[y z].include?(row['playoffs_flag_mlb']),
-            division_id:    row['division_id'].to_i,
-            elim_wildcard:  row['elim_wildcard'],
-            elim:           row['elim'],
-            games_back:     row['gb'].to_f,
-            home_record:    row['home'].split('-'),
-            interleague:    row['interleague'],
-            last_ten:       row['last_ten'],
-            losses:         row['l'].to_i,
-            percent:        row['pct'].to_f,
-            road_record:    row['away'].split('-'),
-            run_diff:       row['runs'].to_i - row['opp_runs'].to_i,
-            streak:         row['streak'],
-            subreddit:      subreddit(row['team_abbrev']),
-            team:           @bot.gameday.team(row['team_abbrev']),
-            wildcard_champ: %w[w x].include?(row['playoffs_flag_mlb']),
-            wildcard_gb:    wildcard(row['gb_wildcard']),
-            wildcard:       row['gb_wildcard'],
-            wins:           row['w'].to_i
-          }.tap do |team|
+            id:             team.id,
+            code:           team.abbreviation,
+            # elim_wildcard:  row['elim_wildcard'],
+            subreddit:      subreddit(team['abbreviation']),
+            division_champ: row['divisionChamp'],
+            elim:           row['eliminationNumber'],
+            games_back:     row['divisionGamesBack'],
+            home_record:    records['home'],
+            last_ten:       records['lastTen'],
+            losses:         row['losses'],
+            percent:        row['leagueRecord']['pct'].to_f,
+            road_record:    records['away'],
+            run_diff:       row['runDifferential'],
+            streak:         row['streak']['streakCode'],
+            team:           team,
+            wildcard_champ: false,
+            wildcard_gb:    row['wildCardGamesBack'],
+            wildcard_rank:  row['wildCardRank'].to_i,
+            wildcard:       row['hasWildcard'] && !row['divisionLeader'],
+            wins:           row['wins']
+          }.tap do |info|
             # Used for sorting teams in the standings. Lowest losing %, most
             # wins, least losses, and then fall back to three letter code
-            team[:sort_order] = [
-              1.0 - team[:percent],
-              162 - team[:wins],
-              team[:losses],
-              team[:code]
+            info[:sort_order] = [
+              1.0 - info[:percent],
+              162 - info[:wins],
+              info[:losses],
+              info[:team]['abbreviation']
             ]
           end
         end
         # rubocop:enable Metrics/MethodLength
 
-        def sort_teams_into_divisions(teams)
-          Hash.new { |hash, key| hash[key] = [] }.tap do |divisions|
-            teams.each_value { |team| divisions[team[:division_id]] << team }
-          end
-        end
-
-        def teams_in_divisions(teams, ids)
-          teams.values.keep_if { |team| ids.include?(team[:division_id]) }
-        end
+        # @!group Wildcards
 
         # Take the eligible teams, remove all teams who aren't at least tied
         # with the team in 5th place, remove teams in first place, and then
         # split between teams ahead of the second spot
         #
         # This might put two teams tied for second instead of tied for first
-        def first_and_second_wildcards(eligible)
-          eligible
-            .reject { |team| team[:wildcard_gb] > eligible[4][:wildcard_gb] }
-            .reject { |team| team[:games_back].zero? }
-            .partition { |team| team[:wildcard_gb].negative? }
+        def mark_league_wildcards(league_id)
+          teams = teams_in_league(league_id)
+
+          division_leaders = teams.count { |team| team[:games_back] == '-' }
+
+          # 5 or more division leaders means no wildcards
+          return [] if division_leaders >= 5
+
+          allowed_wildcards = 5 - division_leaders
+
+          ranked = teams
+            .reject { |team| team[:games_back] == '-' }
+            .sort_by { |team| team[:wildcard_rank] }
+
+          first_wildcard = teams
+            .select { |team| team[:wildcard_rank] == ranked[0][:wildcard_rank] }
+
+          first_wildcard.each { |team| team[:wildcard_position] = 1 }
+
+          return unless first_wildcard.count < allowed_wildcards
+
+          teams
+            .select { |team| team[:wildcard_rank] == ranked[1][:wildcard_rank] }
+            .each { |team| team[:wildcard_position] = 2 }
         end
 
-        def load_teams_from_remote
-          # Don't ask me, MLB started acting stupid one day. Going back 4 hours
-          # seems to fix the problem.
-          filename = (Time.now - 4 * 3600).strftime STANDINGS
+        # @!endgroup Wildcards
 
-          JSON.parse(URI.parse(filename).open.read)
-            .dig(
-              'standings_schedule_date',
-              'standings_all_date_rptr',
-              'standings_all_date'
-            )
-            .flat_map { |league| league['queryResults']['row'] }
+        def load_divisions_from_remote
+          filename = format(
+            STATS_STANDINGS,
+            year: Time.now.year,
+            date: (@subreddit.now + 86_400).strftime('%m/%d/%Y')
+          )
+
+          JSON.parse(URI.parse(filename).open.read).dig('records')
         end
       end
     end
