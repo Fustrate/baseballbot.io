722e7c89bbfb0d910f2d75d01410211d402b3afa
Steven Hoffman <fustrate@me.com>
2018-08-28 13:55:13 -0700

    refactored check_messages script
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- lib/check_messages.rb
+++ lib/check_messages.rb
@@ -3,109 +3,117 @@
 require_relative 'default_bot'
 require 'honeybadger/ruby'
 
-SCHEDULE = \
-  'https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=%<date>s&' \
-  'hydrate=game(content(summary)),linescore,flags,team'
+class CheckMessages
+  SCHEDULE = \
+    'https://statsapi.mlb.com/api/v1/schedule?sportId=1&date=%<date>s&' \
+    'hydrate=game(content(summary)),linescore,flags,team'
 
-TITLE = /(?:game ?(?:thread|chat|day)|gdt)/i
-LINK = %r{(?:redd\.it|/comments|reddit\.com)/([a-z0-9]{6})}i
-GID = /(?:gid_)?(\d{4}_\d{2}_\d{2}_[a-z]{6}_[a-z]{6}_\d)/
+  TITLE = /(?:game ?(?:thread|chat|day)|gdt)/i
+  LINK = %r{(?:redd\.it|/comments|reddit\.com)/([a-z0-9]{6})}i
+  GID = /(?:gid_)?(\d{4}_\d{2}_\d{2}_[a-z]{6}_[a-z]{6}_\d)/
 
-@bot = default_bot(purpose: 'Messages', account: 'BaseballBot')
+  def initialize
+    @bot = default_bot(purpose: 'Messages', account: 'BaseballBot')
+  end
 
-def process_message(message)
-  return unless message.subject =~ TITLE && message.body =~ LINK
+  def run!(retry_on_failure: true)
+    unread_messages.each { |message| process_message message }
+  rescue Redd::APIError
+    return unless retry_on_failure
 
-  post_id = Regexp.last_match[1]
+    puts 'Service unavailable: waiting 30 seconds to retry.'
 
-  submissions = @bot.session.from_ids("t3_#{post_id}")
+    sleep 30
 
-  return unless submissions
+    run!(retry_on_failure: false)
+  rescue => ex
+    Honeybadger.notify(ex)
+  end
 
-  submission = submissions.first
+  protected
 
-  subreddit = submission.subreddit.display_name.downcase
+  def unread_messages
+    @bot.session.my_messages(category: 'unread', mark: false, limit: 10) || []
+  end
 
-  gid = if submission.selftext =~ GID
-          Regexp.last_match[1]
-        else
-          find_possible_game(subreddit, post_id)
-        end
+  def process_message(message)
+    return unless message.subject =~ TITLE && message.body =~ LINK
 
-  @bot.redis.hset gid, subreddit, post_id if gid
+    post_id = Regexp.last_match[1]
 
-  message.mark_as_read
-end
+    submission = @bot.session.from_ids("t3_#{post_id}")&.first
 
-def subreddit_to_code(name)
-  BaseballBot::Subreddits::DEFAULT_SUBREDDITS
-    .find { |_, subreddit| subreddit.casecmp(name).zero? }[0]
-end
+    return unless submission
 
-def find_possible_game(subreddit, post_id)
-  gids = possible_games[subreddit_to_code(subreddit)]
-  possibilities = []
+    subreddit = submission.subreddit.display_name.downcase
 
-  # Remove GIDs that have already been sent to us
-  gids.each do |gid|
-    existing_id = @bot.redis.hget gid, subreddit
+    gid = gid_for_submission(submission, subreddit, post_id)
 
-    # This post_id is already in the system, so make sure `first` returns nil
-    if existing_id == post_id
-      possibilities.unshift nil
-      break
+    @bot.redis.hset gid, subreddit, post_id if gid
+
+    message.mark_as_read
+  end
+
+  def gid_for_submission(submission, subreddit, post_id)
+    return Regexp.last_match[1] if submission.selftext =~ GID
+
+    find_possible_game(subreddit, post_id)
+  end
+
+  def subreddit_to_code(name)
+    BaseballBot::Subreddits::DEFAULT_SUBREDDITS
+      .find { |_, subreddit| subreddit.casecmp(name).zero? }[0]
+  end
+
+  def find_possible_game(subreddit, post_id)
+    gids = possible_games[subreddit_to_code(subreddit)]
+    possibilities = []
+
+    # Remove GIDs that have already been sent to us
+    gids.each do |gid|
+      existing_id = @bot.redis.hget gid, subreddit
+
+      # This post_id is already in the system
+      return nil if existing_id == post_id
+
+      possibilities << gid unless existing_id
     end
 
-    possibilities << gid unless existing_id
+    possibilities.first
   end
 
-  possibilities.first
-end
-
-def possible_games
-  @possible_games ||= load_possible_games
-end
-
-def load_possible_games
-  games = Hash.new { |h, k| h[k] = [] }
-
-  data = URI.parse(
-    format(SCHEDULE, date: Time.now.strftime('%m/%d/%Y'))
-  ).open.read
-
-  JSON.parse(data).dig('dates', 0, 'games').each do |game|
-    # This is no longer included in the data - we might have to switch to
-    # using game_pk instead.
-    gid = [
-      Time.now.strftime('%Y_%m_%d'),
-      "#{game.dig('teams', 'away', 'team', 'teamCode')}mlb",
-      "#{game.dig('teams', 'home', 'team', 'teamCode')}mlb",
-      game['gameNumber']
-    ].join('_')
-
-    games[game.dig('teams', 'away', 'team', 'abbreviation')] << gid
-    games[game.dig('teams', 'home', 'team', 'abbreviation')] << gid
+  def possible_games
+    @possible_games ||= load_possible_games
   end
 
-  games
+  def load_possible_games
+    games = Hash.new { |h, k| h[k] = [] }
+
+    data = URI.parse(
+      format(SCHEDULE, date: Time.now.strftime('%m/%d/%Y'))
+    ).open.read
+
+    JSON.parse(data).dig('dates', 0, 'games').each do |game|
+      gid = gid_for_game(game)
+
+      games[game.dig('teams', 'away', 'team', 'abbreviation')] << gid
+      games[game.dig('teams', 'home', 'team', 'abbreviation')] << gid
+    end
+
+    games
+  end
+
+  # This is no longer included in the data - we might have to switch to
+  # using game_pk instead.
+  def gid_for_game(game)
+    format(
+      '%<date>s_%<away>smlb_%<home>smlb_%<number>d',
+      date: Time.now.strftime('%Y_%m_%d'),
+      away: game.dig('teams', 'away', 'team', 'teamCode'),
+      home: game.dig('teams', 'home', 'team', 'teamCode'),
+      number: game['gameNumber'].to_i
+    )
+  end
 end
 
-def unread_messages
-  @bot.session.my_messages(category: 'unread', mark: false, limit: 5) || []
-end
-
-def check_messages(retry_on_failure: true)
-  unread_messages.each { |message| process_message message }
-rescue Redd::APIError
-  return unless retry_on_failure
-
-  puts 'Service unavailable: waiting 30 seconds to retry.'
-
-  sleep 30
-
-  check_messages(retry_on_failure: false)
-rescue => ex
-  Honeybadger.notify(ex)
-end
-
-check_messages
+CheckMessages.new.run!
