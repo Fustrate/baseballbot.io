cd3831aa59caa1f886fcf72fd7096e1c7d640648
Steven Hoffman <git@fustrate.com>
2018-08-31 10:06:32 -0700

    code cleanup
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/template_refinements.rb
+++ lib/baseballbot/template/template_refinements.rb
@@ -9,15 +9,13 @@ module TemplateRefinements
     def ordinal
       abs_number = to_i.abs
 
-      if (11..13).cover?(abs_number % 100)
-        'th'
-      else
-        case abs_number % 10
-        when 1 then 'st'
-        when 2 then 'nd'
-        when 3 then 'rd'
-        else        'th'
-        end
+      return 'th' if (11..13).cover?(abs_number % 100)
+
+      case abs_number % 10
+      when 1 then 'st'
+      when 2 then 'nd'
+      when 3 then 'rd'
+      else        'th'
       end
     end
   end
