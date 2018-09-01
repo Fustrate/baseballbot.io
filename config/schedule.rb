e786353ffbff75ba2c4a90a6c8d0593d16702d48
Steven Hoffman <git@fustrate.com>
2018-08-31 19:13:11 -0700

    schedule updating posted threads
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,12 @@ set :output, '/home/baseballbot/apps/baseballbot.io/shared/log/whenever.log'
 DIRECTORY = '/home/baseballbot/apps/baseballbot.io/current/lib'
 BUNDLE_EXEC = 'bundle exec'
 
+def step_minutes_by(step, except: [])
+  every "#{(0.step(59, step).to_a - Array(except)).join(',')} * * * *" do
+    yield
+  end
+end
+
 every 1.hour do
   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update"
   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb off_day"
@@ -21,12 +27,16 @@ every 5.minutes do
 end
 
 # So we don't run twice on the hour
-every '5,10,15,20,25,30,35,40,45,50,55 * * * *' do
+step_minutes_by(5, except: 0) do
   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update baseball"
 end
 
-every 2.minutes do
+step_minutes_by(2, except: [0, 30]) do
   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update"
+end
+
+step_minutes_by(30) do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update posted"
 end
 
 every :saturday do
