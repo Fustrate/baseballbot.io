71490fc986405432477a1e875f0ff0f2f9eb5110
Steven Hoffman <git@fustrate.com>
2018-10-29 15:16:10 -0700

    turn everything off
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -15,33 +15,33 @@ end
 #   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby no_hitter_bot.rb"
 # end
 
-every 1.hour do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update"
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb off_day"
-end
+# every 1.hour do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update"
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb off_day"
+# end
 
-every 15.minutes do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby check_messages.rb"
+# every 15.minutes do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby check_messages.rb"
+#
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb pregame"
+# end
 
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb pregame"
-end
-
-every 5.minutes do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb post"
-end
+# every 5.minutes do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb post"
+# end
 
 # So we don't run twice on the hour
-step_minutes_by(5, except: 0) do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update baseball"
-end
+# step_minutes_by(5, except: 0) do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update baseball"
+# end
 
-step_minutes_by(2, except: [0, 30]) do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update"
-end
+# step_minutes_by(2, except: [0, 30]) do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update"
+# end
 
-step_minutes_by(30) do
-  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update posted"
-end
+# step_minutes_by(30) do
+#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update posted"
+# end
 
 # every :saturday do
 #   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby load_sunday_game_threads.rb"
