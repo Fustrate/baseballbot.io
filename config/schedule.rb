6cdb1f727cea7e0d72f80bdc546948ef723b432f
Steven Hoffman <git@fustrate.com>
2019-02-21 10:53:42 -0800

    turn some things back on
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/schedule.rb
+++ config/schedule.rb
@@ -20,32 +20,31 @@ end
 #   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb off_day"
 # end
 
-# every 15.minutes do
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby check_messages.rb"
-#
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb pregame"
-# end
+every 15.minutes do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby check_messages.rb"
+  # command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb pregame"
+end
 
 # every 5.minutes do
 #   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb post"
 # end
 
 # So we don't run twice on the hour
-# step_minutes_by(5, except: 0) do
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update baseball"
-# end
+step_minutes_by(5, except: 0) do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby sidebars.rb update baseball"
+end
 
-# step_minutes_by(2, except: [0, 30]) do
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update"
-# end
+step_minutes_by(2, except: [0, 30]) do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update"
+end
 
-# step_minutes_by(30) do
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update posted"
-# end
+step_minutes_by(30) do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby game_threads.rb update posted"
+end
 
-# every :saturday do
-#   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby load_sunday_game_threads.rb"
-# end
+every :saturday do
+  command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby load_sunday_game_threads.rb"
+end
 
 every 1.day, at: '5:30 am' do
   command "cd #{DIRECTORY} && #{BUNDLE_EXEC} ruby around_the_horn.rb"
