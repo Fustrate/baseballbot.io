04ec998a801968d980e2afe35f3967e614343ff8
Steven Hoffman <shoffman@valenciamgmt.com>
2019-02-21 14:28:06 -0800

    camelize pagination params
    Signed-off-by: Steven Hoffman <shoffman@valenciamgmt.com>

--- app/javascript/fustrate/components/pagination.js
+++ app/javascript/fustrate/components/pagination.js
@@ -9,34 +9,32 @@ const settings = {
 };
 
 class Pagination extends Component {
-  /* eslint-disable camelcase */
   constructor({
-    current_page, total_pages, total_entries, per_page,
+    currentPage, totalPages, totalEntries, perPage,
   }) {
     super();
 
-    this.current_page = current_page;
-    this.total_pages = total_pages;
-    this.total_entries = total_entries;
-    this.per_page = per_page;
+    this.currentPage = currentPage;
+    this.totalPages = totalPages;
+    this.totalEntries = totalEntries;
+    this.perPage = perPage;
 
     this.base = this.constructor.getPreppedPaginationURL();
   }
-  /* eslint-enable camelcase */
 
   link(text, page, ...args) {
     return Fustrate.linkTo(text, `${this.base}page=${page}`, ...args);
   }
 
   previousLink() {
-    if (this.current_page === 1) {
+    if (this.currentPage === 1) {
       return `
         <li class="previous_page unavailable">
           <a href="#">${settings.previousText}</a>
         </li>`;
     }
 
-    const previous = this.link(settings.previousText, this.current_page - 1, {
+    const previous = this.link(settings.previousText, this.currentPage - 1, {
       rel: 'prev',
     });
 
@@ -44,14 +42,14 @@ class Pagination extends Component {
   }
 
   nextLink() {
-    if (this.current_page < this.total_pages) {
+    if (this.currentPage < this.totalPages) {
       return `
         <li class="next_page unavailable">
           <a href="#">${settings.nextText}</a>
         </li>`;
     }
 
-    const next = this.link(settings.nextText, this.current_page + 1, {
+    const next = this.link(settings.nextText, this.currentPage + 1, {
       rel: 'next',
     });
 
@@ -59,12 +57,12 @@ class Pagination extends Component {
   }
 
   generate() {
-    if (this.total_pages === 1) {
+    if (this.totalPages === 1) {
       return $('<ul class="pagination">');
     }
 
     const pages = this.windowedPageNumbers().map((page) => {
-      if (page === this.current_page) {
+      if (page === this.currentPage) {
         return `<li class="current">${Fustrate.linkTo(page, '#')}</li>`;
       }
 
@@ -84,20 +82,20 @@ class Pagination extends Component {
   windowedPageNumbers() {
     let pages = [];
 
-    let windowFrom = this.current_page - 4;
-    let windowTo = this.current_page + 4;
+    let windowFrom = this.currentPage - 4;
+    let windowTo = this.currentPage + 4;
 
-    if (windowTo > this.total_pages) {
-      windowFrom -= windowTo - this.total_pages;
-      windowTo = this.total_pages;
+    if (windowTo > this.totalPages) {
+      windowFrom -= windowTo - this.totalPages;
+      windowTo = this.totalPages;
     }
 
     if (windowFrom < 1) {
       windowTo += 1 - windowFrom;
       windowFrom = 1;
 
-      if (windowTo > this.total_pages) {
-        windowTo = this.total_pages;
+      if (windowTo > this.totalPages) {
+        windowTo = this.totalPages;
       }
     }
 
@@ -113,12 +111,12 @@ class Pagination extends Component {
       pages.push(i);
     }
 
-    if (this.total_pages - 3 > pages[pages.length - 1]) {
+    if (this.totalPages - 3 > pages[pages.length - 1]) {
       pages.push('gap');
-      pages.push(this.total_pages - 1);
-      pages.push(this.total_pages);
-    } else if (pages[pages.length - 1] + 1 <= this.total_pages) {
-      for (let i = pages[pages.length - 1] + 1; i <= this.total_pages; i += 1) {
+      pages.push(this.totalPages - 1);
+      pages.push(this.totalPages);
+    } else if (pages[pages.length - 1] + 1 <= this.totalPages) {
+      for (let i = pages[pages.length - 1] + 1; i <= this.totalPages; i += 1) {
         pages.push(i);
       }
     }
