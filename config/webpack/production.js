fbe256267b1bfaebd1571c8cebea455dbe7a1cb6
Steven Hoffman <git@fustrate.com>
2019-03-14 11:54:39 -0700

    Disable sourcemaps and mangling
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,38 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const environment = require('./environment');
 
-module.exports = environment.toWebpackConfig();
+const smp = new SpeedMeasurePlugin();
+
+// Disable sourcemaps and mangling
+const terser = new TerserPlugin({
+  parallel: true,
+  cache: true,
+  sourceMap: false,
+  terserOptions: {
+    parse: {
+      // Let terser parse ecma 8 code but always output ES5 compliant code for older browsers
+      ecma: 8,
+    },
+    compress: {
+      comparisons: false,
+      defaults: false,
+      ecma: 5,
+      keep_classnames: true,
+      keep_fnames: true,
+      warnings: false,
+    },
+    mangle: false,
+    output: {
+      ecma: 5,
+      comments: false,
+      ascii_only: true,
+    },
+  },
+});
+
+environment.config.optimization.minimizer = [terser];
+
+module.exports = smp.wrap(environment.toWebpackConfig());
