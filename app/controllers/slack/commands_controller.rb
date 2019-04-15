1eda2507b9db74514cc86c74ac1bd06fe5837235
Steven Hoffman <git@fustrate.com>
2019-04-14 17:38:22 -0700

    hmm
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- app/controllers/slack/commands_controller.rb
+++ app/controllers/slack/commands_controller.rb
@@ -4,7 +4,13 @@ module Slack
   class CommandsController < ApplicationController
     before_action :verify_slack_signature
 
+    # We don't need CSRF protection here
+    protect_from_forgery with: :null_session
+
     def gdt
+      # params[:command] => '/gdt
+      # params[:text] => 'test'
+
       render plain: 'Okay', status: 200
     end
 
