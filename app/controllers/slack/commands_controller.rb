5ea785857fd5fa00d18a5427a5248bb3ad9b7f12
Steven Hoffman <git@fustrate.com>
2019-04-20 15:29:01 -0700

    extract out gdt command
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- app/controllers/slack/commands_controller.rb
+++ app/controllers/slack/commands_controller.rb
@@ -8,22 +8,7 @@ module Slack
     protect_from_forgery with: :null_session
 
     def gdt
-      # params[:command] => '/gdt
-      # params[:text] => 'add angels 4/19'
-
-      command, args = params[:text].split(' ', 2)
-
-      case command
-      when 'add' then add_gdt(args)
-      when 'list' then list_gdt(args)
-      else
-        render plain: '???', status: 200
-      end
-    end
-
-    def add_gdt(_text)
-      # if text.match?(//)
-      render plain: 'Add', status: 200
+      Slack::Commands::GDT.call
     end
 
     protected
---
+++ app/services/slack/commands/gdt.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module Slack
+  module Commands
+    class GDT < ApplicationService
+      POSTGAME_STATUSES = /Final|Game Over|Postponed|Completed Early/.freeze
+
+      def call
+        # params[:command] => '/gdt
+        # params[:text] => 'add angels 4/19'
+
+        command, args = params[:text].split(' ', 2)
+
+        case command
+        when 'add' then add_gdt(args)
+        when 'list' then list_gdt(args)
+        else
+          '???'
+        end
+      end
+
+      protected
+
+      def add_gdt(text)
+        case text
+        when /free(?: game)?(?<date> .*)?/ then add_free_game(Regexp.last_match)
+        when /\A\d{6}\z/ then add_game_by_pk(text.to_i)
+        else
+          'Only free games are supported so far.'
+        end
+      end
+
+      def list_gdt(_text)
+        'listing upcoming gdts'
+      end
+
+      def add_free_game(args)
+        free_games = find_games(args[:date])
+          .select { |game| game.dig('content', 'media', 'freeGame') }
+
+        return 'There is more than one free game.' if free_games.count > 1
+        return 'There are no free games.' if free_games.none?
+
+        add_game_by_pk free_games.dig(0, 'gamePk')
+
+        "Adding #{game.dig('teams', 'away', 'team', 'teamName')} @ " \
+          "#{game.dig('teams', 'home', 'team', 'teamName')} to the list"
+      end
+
+      def add_game_by_pk(game_pk)
+        live_feed = api.live_feed(game_pk)
+
+        return 'Invalid game PK' unless game
+
+        status = live_feed.game_data.dig('status', 'abstractGameState')
+
+        if ['Live', 'Final']
+      end
+
+      def find_games(text)
+        date = text && text != 'today' ? Chronic.parse(text) : Time.zone.today
+
+        api.schedule(
+          sportId: 1,
+          date: date.strftime('%m/%d/%Y'),
+          eventTypes: 'primary',
+          scheduleTypes: 'games'
+        ).dig('dates', 0, 'games') || []
+      end
+
+      def api
+        @api ||= MLBStatsAPI::Client.new(
+          logger: Rails.logger,
+          cache: Rails.application.redis
+        )
+      end
+    end
+  end
+end
