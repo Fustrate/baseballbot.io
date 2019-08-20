1823552e9f0bc4cdd343028b8143bad65f505fd8
Steven Hoffman <git@fustrate.com>
2019-08-20 13:30:30 -0700

    move some things around
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- lib/baseballbot/template/shared/calendar.rb
+++ lib/baseballbot/template/shared/calendar.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative '../markdown_calendar'
-require_relative '../team_calendar'
+require_relative 'markdown_calendar'
+require_relative 'team_calendar'
 
 class Baseballbot
   module Template
