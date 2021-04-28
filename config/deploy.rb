8087dc69e859666526fd7de09e7fbcc34c23a6f1
Steven Hoffman <git@fustrate.com>
2021-04-27 22:41:11 -0700

    don’t link the node_modules dir
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,11 +10,10 @@ set :deploy_to, "/home/#{fetch :user}/apps/#{fetch :application}"
 set :repo_url, 'git@github.com:Fustrate/baseballbot.io.git'
 set :branch, ENV['REVISION'] || :master
 
-append :linked_dirs, 'log', 'node_modules', 'public/packs', 'public/system', 'tmp/cache',
-       'tmp/pids', 'tmp/sockets'
+append :linked_dirs, 'log', 'public/packs', 'public/system', 'tmp/cache', 'tmp/pids', 'tmp/sockets'
 
-append :linked_files, 'config/database.yml', 'config/honeybadger.yml', 'config/master.key',
-       'config/reddit.yml', 'config/skylight.yml'
+append :linked_files, 'config/database.yml', 'config/honeybadger.yml', 'config/master.key', 'config/reddit.yml',
+       'config/skylight.yml'
 
 set :default_env, path: '/opt/ruby/bin:$PATH'
 
