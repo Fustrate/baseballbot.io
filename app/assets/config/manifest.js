c2b050fae6ebe9f3434da14d11d4bd499e360b75
Fustrate <fustrate@me.com>
2022-01-07 10:36:59 -0800

    trying to make FA6 fonts work

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,1 +1,4 @@
 //= link_tree ../builds
+
+// Font Awesome
+//= link_tree ../../../node_modules/@fortawesome/fontawesome-pro/webfonts
---
+++ config/initializers/assets.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/webfonts')
+
+module Sprockets
+  module Rails
+    # Resolve assets referenced in CSS `url()` calls and replace them with the digested paths
+    class AssetUrlProcessor
+      REGEX = /url\(\s*["']?(?!(?:\#|data|http))(?<relativeToCurrentDir>\.\/)?(?<path>[^"'\s)]+)\s*["']?\)/
+
+      def self.call(input)
+        context = input[:environment].context_class.new(input)
+
+        data = input[:data].gsub(REGEX) do |_match|
+          path = Regexp.last_match[:path]
+
+          Rails.logger.info "MATCH MATCH MATCH #{path} = #{context.asset_path(path)}"
+
+          "url(#{context.asset_path(path)})"
+        end
+
+        context.metadata.merge(data: data)
+      end
+    end
+  end
+end
