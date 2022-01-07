00f6aef2f0dc7cfdb442c6355733c5055e0c7259
Fustrate <fustrate@me.com>
2022-01-07 11:56:57 -0800

    hmm

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,15 +6,13 @@ module Sprockets
   module Rails
     # Resolve assets referenced in CSS `url()` calls and replace them with the digested paths
     class AssetUrlProcessor
-      REGEX = /url\(\s*["']?(?!(?:\#|data|http))(?<relativeToCurrentDir>\.\/)?(?<path>[^"'\s)]+)\s*["']?\)/
-
       def self.call(input)
         context = input[:environment].context_class.new(input)
 
         data = input[:data].gsub(REGEX) do |_match|
           path = Regexp.last_match[:path]
 
-          Rails.logger.info "MATCH MATCH MATCH #{path} = #{context.asset_path(path)}"
+          ::Rails.logger.info "MATCH MATCH MATCH #{path} = #{context.asset_path(path)}"
 
           "url(#{context.asset_path(path)})"
         end
