5be455222a9b8e831dbe733b363a37500e536d6e
Fustrate <fustrate@me.com>
2022-01-12 12:56:26 -0800

    ???

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.0.1'
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/fontawesome-pro/webfonts')
