b8c73ba0b3929e1aec1f6ec6c703a3d1a2226389
Steven Hoffman <fustrate@me.com>
2022-05-06 09:28:22 -0700

    validations for game threads
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/models/game_thread.rb
+++ app/models/game_thread.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class GameThread < ApplicationRecord
-  belongs_to :subreddit
+  belongs_to :subreddit, optional: false
+
+  TYPES = %w[no_hitter game_thread].freeze
+  STATUSES = %w[Future Pregame Posted Over Removed Postponed Foreign].freeze
+
+  validates :game_pk, :post_at, :starts_at, :status, presence: true
+  validates :type, inclusion: TYPES, allow_nil: true
+  validates :status, inclusion: STATUSES
 end
