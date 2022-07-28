ca34c184b09f5ef982a4fd25cc0785653e35ce2d
Steven Hoffman <fustrate@me.com>
2022-07-27 21:49:53 -0700

    fixes & compact
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/services/subreddits/update.rb
+++ app/services/subreddits/update.rb
@@ -4,12 +4,15 @@ module Subreddits
   class Update < ApplicationService
     def call(subreddit)
       @subreddit = subreddit
+      @options = params.dig(:subreddit, :options) || {}
 
       # Eventually the whole options hash will be assigned at once instead of this piecemeal method
       update_game_thread_settings
       update_pregame_settings
       update_postgame_settings
       update_off_day_settings
+
+      @subreddit.options.compact!
 
       @subreddit.save!
     end
@@ -18,22 +21,30 @@ module Subreddits
 
     def update_game_thread_settings
       @subreddit.options['game_threads'] ||= {}
-      @subreddit.options['game_threads']['sticky_comment'] = params.dig(:subreddit, :game_threads, :sticky_comment)
+      @subreddit.options['game_threads']['sticky_comment'] = @options.dig(:game_threads, :sticky_comment)
+
+      @subreddit.options['game_threads'].compact!
     end
 
     def update_pregame_settings
       @subreddit.options['pregame'] ||= {}
-      @subreddit.options['pregame']['sticky_comment'] = params.dig(:subreddit, :pregame, :sticky_comment)
+      @subreddit.options['pregame']['sticky_comment'] = @options.dig(:pregame, :sticky_comment)
+
+      @subreddit.options['pregame'].compact!
     end
 
     def update_postgame_settings
       @subreddit.options['postgame'] ||= {}
-      @subreddit.options['postgame']['sticky_comment'] = params.dig(:subreddit, :postgame, :sticky_comment)
+      @subreddit.options['postgame']['sticky_comment'] = @options.dig(:postgame, :sticky_comment)
+
+      @subreddit.options['postgame'].compact!
     end
 
     def update_off_day_settings
       @subreddit.options['off_day'] ||= {}
-      @subreddit.options['off_day']['sticky_comment'] = params.dig(:subreddit, :off_day, :sticky_comment)
+      @subreddit.options['off_day']['sticky_comment'] = @options.dig(:off_day, :sticky_comment)
+
+      @subreddit.options['off_day'].compact!
     end
   end
 end
