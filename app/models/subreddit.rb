b3e86602691df003b37d3b249cd19c0d82982002
Steven Hoffman <fustrate@me.com>
2022-07-27 21:34:08 -0700

    remove unused methods
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/models/subreddit.rb
+++ app/models/subreddit.rb
@@ -15,22 +15,6 @@ class Subreddit < ApplicationRecord
 
   def url = "https://reddit.com/r/#{name}"
 
-  def update_sidebar?
-    @update_sidebar ||= options.dig('sidebar', 'enabled')
-  end
-
-  def post_game_threads?
-    @post_game_threads ||= options.dig('game_threads', 'enabled')
-  end
-
-  def post_pregame?
-    @post_pregame ||= options.dig('pregame', 'enabled')
-  end
-
-  def post_postgame?
-    @post_postgame ||= options.dig('postgame', 'enabled')
-  end
-
   # TODO: These are required for SubredditAuror to work. Make it work without them.
 
   def subreddit = self
