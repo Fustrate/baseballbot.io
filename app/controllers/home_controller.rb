383ba1a2fdd22111f767fd418ebbc347830af7f8
Steven Hoffman <fustrate@me.com>
2023-04-01 09:26:35 -0700

    order threads on home page
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,22 @@
 
 # Home page, full of nothing right now
 class HomeController < ApplicationController
+  # Sort by today's games, posted games, future games,
+  DEFAULT_ORDER = Arel.sql(<<~SQL.squish)
+    status = 'Posted' DESC,
+    status = 'Pregame' DESC,
+    status = 'Future' DESC,
+    status IN ('Over', 'Postponed', 'Removed') ASC,
+    status = 'External' ASC,
+    post_at > NOW() DESC,
+    CASE WHEN post_at > NOW() THEN -(NOW() - starts_at) ELSE NOW() - starts_at END ASC
+  SQL
+
   def home
     @game_threads = GameThread
       .where('DATE(post_at) = ?', Time.zone.today)
       .includes(:subreddit)
+      .order(DEFAULT_ORDER)
   end
 
   def gameday
