eca7f78496dd05ee8bbf185db667015caaeccc9a
Steven Hoffman <fustrate@me.com>
2025-06-29 22:00:34 -0700

    remove unused files
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/frontend/javascript/components/generic-show.ts
+++
@@ -1,5 +0,0 @@
-import GenericPage from '@fustrate/rails/generic-page';
-
-export { refresh } from '@fustrate/rails/generic-page';
-
-export default abstract class GenericShow extends GenericPage {}
