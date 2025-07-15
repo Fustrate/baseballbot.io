ba1978af1026e9dd2c466c9065ddb3a4a4d485e0
Steven Hoffman <fustrate@me.com>
2025-07-15 10:57:21 -0700

    remove unused method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   include SetCurrentRequestDetails
   include HandleExceptions
   include Authenticate
-
-  def self.restrict_formats(format, **options)
-    before_action(options) do
-      raise ActionController::UnknownFormat unless request.format.symbol == format
-    end
-  end
 end
