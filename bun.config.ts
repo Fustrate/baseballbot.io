ece2df1b66bd579545a50f07f8532dfc065f639c
Steven Hoffman <fustrate@me.com>
2026-01-09 09:12:02 -0800

    building with bun might work now
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- bun.config.ts
+++ bun.config.ts
@@ -1,40 +1,31 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import Bun from 'bun';
 
-import { TanStackRouterCodeSplitterEsbuild } from '@tanstack/router-plugin/esbuild';
-
-const config = {
+// Tanstack's code splitting doesn't work with Bun yet - it looks like unplugin v3 is still in beta, after which
+// @tanstack/router-plugin will need to be updated to support v3 and Bun.
+const config: Bun.BuildConfig = {
   entrypoints: ['app/frontend/entrypoints/application.tsx'],
   outdir: 'app/assets/builds',
   minify: true,
-  sourcemap: 'none',
-  target: 'browser',
   splitting: true,
   naming: {
     // Append '.digested' so that Propshaft doesn't add its own digest and lose the files
     entry: '[dir]/[name].[ext]',
-    chunk: '[name]-[hash].digested.[ext]',
+    chunk: 'chunk-[hash].digested.[ext]',
     asset: '[name]-[hash].digested.[ext]',
   },
-  plugins: [
-    TanStackRouterCodeSplitterEsbuild({
-      target: 'react',
-      autoCodeSplitting: true,
-      routesDirectory: './app/frontend/routes',
-      generatedRouteTree: './app/frontend/routeTree.gen.ts',
-    }),
-  ],
-  define: {
-    global: 'window',
-  },
+  // define: {
+  //   global: 'window',
+  // },
 };
 
-const build = async (config) => {
+const build = async (config: Bun.BuildConfig) => {
   const result = await Bun.build(config);
 
   if (!result.success) {
     if (process.argv.includes('--watch')) {
-      console.error("Build failed");
+      console.error('Build failed');
 
       for (const message of result.logs) {
         console.error(message);
@@ -43,7 +34,7 @@ const build = async (config) => {
       return;
     }
 
-    throw new AggregateError(result.logs, "Build failed");
+    throw new AggregateError(result.logs, 'Build failed');
   }
 };
 
@@ -51,7 +42,7 @@ const build = async (config) => {
   await build(config);
 
   if (process.argv.includes('--watch')) {
-    fs.watch(path.join(process.cwd(), "app/frontend"), { recursive: true }, (_eventType, filename) => {
+    fs.watch(path.join(process.cwd(), 'app/frontend'), { recursive: true }, (_eventType, filename) => {
       if (filename.endsWith('.ts') || filename.endsWith('.tsx')) {
         console.log(`File changed: ${filename}. Rebuilding...`);
         build(config);
