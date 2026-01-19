e075222d1a45306a4d2cba265891c92ff620dc1a
Steven Hoffman <fustrate@me.com>
2026-01-19 14:08:28 -0800

    sort moderated subs to the top

--- app/frontend/routes/subreddits/index.tsx
+++ app/frontend/routes/subreddits/index.tsx
@@ -4,6 +4,7 @@ import { fetchSubreddits, type Subreddit } from '@/api/subreddits';
 import { Heading } from '@/catalyst/heading';
 import { Link } from '@/catalyst/link';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/catalyst/table';
+import { useAuth } from '@/hooks/useAuth';
 import { postAtFormat } from '@/utilities';
 
 export const Route = createFileRoute('/subreddits/')({
@@ -16,6 +17,20 @@ export const Route = createFileRoute('/subreddits/')({
 
 function RouteComponent() {
   const subreddits = Route.useLoaderData();
+  const { isLoggedIn, user } = useAuth();
+
+  const moderatedSubIds = user?.subreddits || [];
+
+  const sortedSubreddits = [...subreddits].sort((a, b) => {
+    const aModerated = moderatedSubIds.includes(a.id);
+    const bModerated = moderatedSubIds.includes(b.id);
+
+    if (aModerated !== bModerated) {
+      return aModerated ? -1 : 1;
+    }
+
+    return a.name.localeCompare(b.name);
+  });
 
   return (
     <>
@@ -27,8 +42,10 @@ function RouteComponent() {
         <TableHead>
           <TableRow>
             <TableHeader>Name</TableHeader>
+            {isLoggedIn && <TableHeader />}
             <TableHeader>Team</TableHeader>
             <TableHeader>Account</TableHeader>
+            <TableHeader>Sidebar</TableHeader>
             <TableHeader>Game Threads</TableHeader>
             <TableHeader>Pre Game</TableHeader>
             <TableHeader>Post Game</TableHeader>
@@ -37,13 +54,18 @@ function RouteComponent() {
         </TableHead>
 
         <TableBody>
-          {subreddits.map((subreddit: Subreddit) => (
+          {sortedSubreddits.map((subreddit: Subreddit) => (
             <TableRow key={subreddit.id}>
               <TableCell>
                 <Link inline to="/subreddits/$subredditId" params={{ subredditId: subreddit.name }}>
                   {subreddit.name}
                 </Link>
               </TableCell>
+              {isLoggedIn && (
+                <TableCell>
+                  {moderatedSubIds.includes(subreddit.id) && <i className="fa fa-star" title="Moderator" />}
+                </TableCell>
+              )}
               <TableCell>{subreddit.abbreviation}</TableCell>
               <TableCell>{subreddit.bot.name}</TableCell>
               <TableCell>{subreddit.options.sidebar?.enabled && <i className="fa fa-check" />}</TableCell>
