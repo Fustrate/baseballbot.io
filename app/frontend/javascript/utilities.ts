f6b816cbdea143f8b776b7b37f4244f519dece6c
Steven Hoffman <fustrate@me.com>
2026-01-19 20:50:37 -0800

    change postAtFormat to use team’s time zone
    Signed-off-by: Steven Hoffman <fustrate@me.com>

--- app/frontend/javascript/utilities.ts
+++ app/frontend/javascript/utilities.ts
@@ -1,7 +1,8 @@
 import clsx, { type ClassValue } from 'clsx';
+import { DateTime } from 'luxon';
 import { twMerge } from 'tailwind-merge';
 
-export function postAtFormat(postAt?: string): string {
+export function postAtFormat(postAt?: string, inTimeZone?: string): string {
   if (!postAt) {
     return '3 Hours Pregame';
   }
@@ -10,8 +11,12 @@ export function postAtFormat(postAt?: string): string {
     return `${Math.abs(Number.parseInt(postAt, 10))} Hours Pregame`;
   }
 
+  // This needs to be converted to the team's time zone for display purposes
   if (/(1[0-2]|\d)(:\d\d|) ?(am|pm)?/i.test(postAt)) {
-    return `at ${postAt}`;
+    return DateTime.fromFormat(postAt, 'h:mm', { zone: 'America/Los_Angeles' })
+      .setZone(inTimeZone || 'America/Los_Angeles')
+      .toFormat('h:mm a ZZZZ')
+      .replace(':00', '');
   }
 
   // Bad format, default back to 3 hours pregame
